bad87ff6ece14d66754bb9ceb187c90f4ec00d0d
Joshua K. Farrar <joshua@sent1nel.me>
2015-08-21 11:09:37 -0400

    gulpfile updates (environmental conditionals)

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var uglify = require('gulp-uglify');
+var beautify = require('gulp-beautify');
 var rev = require('gulp-rev');
 var inject = require('gulp-inject');
+var gulpif = require('gulp-if');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var del = require('del');
+
+process.env.NODE_ENV = process.env.NODE_ENV || ''
 
 gulp.task('build', function () {
   var jsStream = browserify({
@@ -16,10 +20,16 @@ gulp.task('build', function () {
   })
   .transform(babelify)
   .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(buffer())
-  .pipe(uglify())
-  .pipe(rev())
+  .pipe(
+      gulpif(
+        (process.env.NODE_ENV == 'production'),
+        source('bundle.min.js'),
+        source('bundle.js')
+      )
+  )
+  .pipe(gulpif((process.env.NODE_ENV == 'production'), buffer()))
+  .pipe(gulpif((process.env.NODE_ENV == 'production'), uglify()))
+  .pipe(gulpif((process.env.NODE_ENV == 'production'), rev()))
   .pipe(gulp.dest('dist/js'));
 
   return gulp.src('client/index.html')
